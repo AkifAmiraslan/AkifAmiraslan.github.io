07afd5150c982b84dc0c23c74a318bcea1419388
AkifAmiraslan <akifamiraslannnn@gmail.com>
2024-01-23 21:08:42 +0400

    upgraded

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-export {default as HomePage} from './homePage.jsx'
+export {default as HomePage} from './HomePage.jsx'
 export {default as CoursesPage} from './CoursesPage.jsx'
 export {default as AboutUsPage} from './AboutUsPage.jsx'
 export {default as BlogsPage} from './BlogsPage.jsx'
